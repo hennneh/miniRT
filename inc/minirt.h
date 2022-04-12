672ae9ee170ad4791f9ad5084c8d22803f44c9d7
Vincent Heymans <vheymans@c2s17d101.42wolfsburg.de>
2022-04-12 16:30:45 +0200

    a few little changes

--- inc/minirt.h
+++ inc/minirt.h
@@ -1,3 +1,40 @@
 #ifndef MINIRT_H
 # define MINIRT_H
+
+/*
+ * DEFINES
+ */
+
+# define MAX_PATH 1024
+
+/*
+**	LIBRARY
+*/
+
+# include "../lft/libft.h"
+
+/*
+**	INCLUDES
+*/
+
+# include <stdio.h> //  printf
+
+/*
+** MACROS
+*/
+
+/*
+**	STRUCTS
+*/
+
+/*
+ * FUNCTIONS
+*/
+
+//SRC
+
+int		main(int argc, char **argv);
+
+// INPUT
+
 #endif
