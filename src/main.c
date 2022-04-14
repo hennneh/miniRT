8f7c6b4bac62d279fc8013ff9f182a647cb87e62
c8p2d01 <78210806+c8p2d01@users.noreply.github.com>
2022-04-14 17:13:11 +0200

    Revert "hennes (ex)plane(ation)"

--- src/main.c
+++ src/main.c
@@ -1,23 +1,13 @@
 #include "../inc/minirt.h"
 
-int	ft_exit(t_data *img)
-{
-	mlx_destroy_window(img->mlx, img->win);
-	free(img->mlx);
-	exit(0);
-	return (0);
-}
-
 int	main(int argc, char **argv)
 {
-//	void	*mlx;
-//	void	*mlx_win;
-	t_data	img;
+	void	*mlx;
+	void	*mlx_win;
 
-	img.mlx = mlx_init();
-	img.win = mlx_new_window(img.mlx, 1920, 1080, "miniRT");
-	mlx_hook(img.win, 17, 0, ft_exit, &img);
-	mlx_loop(img.mlx);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 	(void) argc;
 	(void) argv;
 //	input_handler(argc, argv);
