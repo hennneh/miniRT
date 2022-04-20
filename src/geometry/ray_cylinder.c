801131ad9f0c0d4c7820197c94480243b910fcc4
Jesse Scheumann <jesse.scheumann@gmx.de>
2022-04-20 17:54:41 +0200

    actual state

--- src/geometry/ray_cylinder.c
+++ src/geometry/ray_cylinder.c
@@ -1,6 +1,6 @@
 #include "../../inc/minirt.h"
 
-double	cylinder_intersect(double *pos, double radius, double height, double *ray_or, double *ray_dir)
+double	cylinder_intersect(double *pos, double *dir, double radius, double height, double *ray_or, double *ray_dir)
 {
 	double	a;
 	double	b;
@@ -9,19 +9,69 @@ double	cylinder_intersect(double *pos, double radius, double height, double *ray
 	double	t1;
 	double	t2;
 
-	a = (ray_dir[0] * ray_dir[0]) + (ray_dir[2] * ray_dir[2]);
-	b = 2 * (ray_dir[0] * (pos[0] * ray_or[0]) + ray_dir[2] * (pos[2] * ray_or[2]));
-	c = (pos[0] - ray_dir[0]) * (pos[0] - ray_or[0]) + (pos[2] - ray_or[2]) * (pos[2] - ray_or[2]) - (radius * radius);
-	delta = b * b - 4 * (4 * c);
-	if (fabs(delta) < 0.001 | delta < 0.0)
+	a = (ray_dir[0] * ray_dir[0]) + (ray_dir[1] * ray_dir[1]);
+	b = 2 * (ray_dir[0] * ray_or[0] + ray_dir[1] * ray_or[1]);		//(ray_dir[0] * (ray_or[0] - pos[0]) + ray_dir[2] * (ray_or[2] - pos[2]));
+	c = ray_or[0] * ray_or[0] + ray_or[1] * ray_or[1] - radius*radius;				//(ray_or[0] - pos[0]) * (ray_or[0] - pos[0]) + (ray_or[2] - pos[2]) * (ray_or[2] - pos[2]) - (radius * radius);
+	delta = b * b - 4 * (a * c);
+	if (fabs(delta) < 0.001)
+	{
+		printf(".");
 		return (-1.0);
+	}
+	//if (delta < 0.0) return -1.0;
 	t1 = (-b - sqrt(delta)) / (2 * a);
 	t2 = (-b + sqrt(delta)) / (2 * a);
-	if (t1 > t2)
+	if (t1 < t2 && t2 < 0)
 		t1 = t2;
-	t2 = pos[1] + t1 * ray_dir[1];
-	if (t2 >= ray_or[1] && t2 <= ray_or[1] + height)
+	//return t1;
+	t2 = ray_or[2] + t1 * ray_dir[2];
+	if (t2 >= pos[2] && t2 <= pos[2] + height/2)
 		return (t1);
 	else
-		return (-1);
+		return (-1.0);
+}
+
+double	*normalize(double *vector)
+{
+	double	len;
+	double	*v;
+
+	v = malloc(sizeof(double) * 3);
+	len = sqrt(pow(vector[0], 2) + pow(vector[1], 2) + pow(vector[2], 2));
+	v[0] = vector[0] / len;
+	v[1] = vector[1] / len;
+	v[2] = vector[2] / len;
+	return (v);
+}
+
+double	*get_vector(double *point_one, double *point_two)
+{
+	double	*vec;
+
+	vec = malloc(sizeof(double) * 3);
+	vec[0] = point_two[0] - point_one[0];
+	vec[1] = point_two[1] - point_one[1];
+	vec[2] = point_two[2] - point_one[2];
+	return (vec);
+}
+
+void	cylinder_coefficient(double *pos, double *dir, double *a, double *ray_or, double *ray_dir)
+{
+	double	*v;
+	double	*n;
+	double	*a;
+
+	n = normalize(dir);
+	v = get_vector(pos, ray_or);
+	a = malloc(sizeof(double) * 3);
+	a[0] = ;
+	a[1] = ;
+	a[2] = ;
+	free(n);
+	free(v);
+}
+
+double	new_cylinder_intersect()
+{
+	
 }
