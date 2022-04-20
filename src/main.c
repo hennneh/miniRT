0f0ccf85a2262e5fb4e26b6133474cd08895fe25
hennneh <hendrik.lehmann96@gmail.com>
2022-04-20 13:54:30 +0200

    fixing function-call order in main

--- src/main.c
+++ src/main.c
@@ -6,11 +6,14 @@ int	main(int argc, char **argv)
 	void	*mlx_win;
 	t_mrt	mrt;
 
+	if (argc != 2)
+	{
+		printf("not exactly one arguemnt\n");
+		return (1);
+	}
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	mlx_loop(mlx);
-	if (argc != 2)
-		return (1);
 	input(&mrt, argv[1]);
 	return (0);
 }
