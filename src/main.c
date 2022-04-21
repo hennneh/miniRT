351ce7279f6cb3a1a1da2908cbb881fb9cb34e57
Vincent Heymans <vheymans@c2s17d101.42wolfsburg.de>
2022-04-21 13:44:15 +0200

    cleaned main

--- src/main.c
+++ src/main.c
@@ -1,18 +1,18 @@
 #include "../inc/minirt.h"
 
-int	print_mrt(t_mrt *m)
-{
-	printf("\nLIGHT\n{\n");
-	printf("id : [%c]\n", m->l->id);
-	printf("cor : [%f][%f][%f]\n", m->l->cor[0], m->l->cor[1], m->l->cor[2]);
-	printf("brit : [%f]\n}\n", m->l->brit);
-	printf("\nSPHERE\n{\n");
-	printf("id : [%c]\n", m->sp[0]->id);
-	printf("cor : [%f][%f][%f]\n", m->sp[0]->cor[0], m->sp[0]->cor[1], m->sp[0]->cor[2]);
-	printf("rad : [%f]\n", m->sp[0]->rad);
-	printf("rgb : [%d][%d][%d]\n}\n", m->sp[0]->r, m->sp[0]->g, m->sp[0]->b);
-	return (1);
-}
+// int	print_mrt(t_mrt *m)
+// {
+// 	printf("\nLIGHT\n{\n");
+// 	printf("id : [%c]\n", m->l->id);
+// 	printf("cor : [%f][%f][%f]\n", m->l->cor[0], m->l->cor[1], m->l->cor[2]);
+// 	printf("brit : [%f]\n}\n", m->l->brit);
+// 	printf("\nSPHERE\n{\n");
+// 	printf("id : [%c]\n", m->sp[0]->id);
+// 	printf("cor : [%f][%f][%f]\n", m->sp[0]->cor[0], m->sp[0]->cor[1], m->sp[0]->cor[2]);
+// 	printf("rad : [%f]\n", m->sp[0]->rad);
+// 	printf("rgb : [%d][%d][%d]\n}\n", m->sp[0]->r, m->sp[0]->g, m->sp[0]->b);
+// 	return (1);
+// }
 
 int	main(int argc, char **argv)
 {
